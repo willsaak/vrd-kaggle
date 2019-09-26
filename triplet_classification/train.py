c68f8ec2e9c2fbb2216b4f5b05ac3b593b0ac9c3
William Saakyan <william@rick.renumics>
2019-09-26 16:03:18 +0200

    move train and config files for relationship classification to folder triplet classification

--- triplet_classification/train.py
+++ triplet_classification/train.py
@@ -5,7 +5,7 @@ import os
 from sklearn.model_selection import train_test_split
 from typing import List, Optional, Tuple
 
-import relationship_detector.config as config
+import triplet_classification.config as config
 from data.data_sequence import DataSequence
 from models.inception_resnet_v2 import InceptionResNetV2
 
